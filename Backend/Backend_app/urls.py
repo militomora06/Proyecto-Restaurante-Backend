f51d39fbf20fea6a5832e590ca01d67f27dc371c
Juan Camilo <juanmororo@unisabana.edu.co>
2025-05-09 14:38:39 -0500

    Reinicio de todo

--- Backend/Backend_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductoListView, PedidoCreateView
-
-urlpatterns = [
-    path('productos/', ProductoListView.as_view(), name='producto-list'),
-    path('pedidos/', PedidoCreateView.as_view(), name='pedido-create'),
-]
