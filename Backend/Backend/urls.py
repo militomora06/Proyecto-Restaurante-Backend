67f41174be198c23be8450c5acc3d781445295a2
AnaSofia64 <anarofe@unisabana.edu.co>
2025-05-23 13:05:11 -0500

    Update urls.py

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
+from Backend_app import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('api/register/', views.registro, name='registro'),
+    path('api/login/', views.login_usuario, name='login_usuario'),
 ]
